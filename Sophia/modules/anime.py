3f3fc788c1f3bbdec50701ffe463114022594789
CALLMEVP <82378057+callmevp@users.noreply.github.com>
2021-08-09 03:03:42 +0530

    Update anime.py

--- Sophia/modules/anime.py
+++ Sophia/modules/anime.py
@@ -5,8 +5,8 @@ import textwrap
 import bs4
 import jikanpy
 import requests
-from Sophia import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
-from Sophia.modules.disable import DisableAbleCommandHandler
+from MissVisa import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
+from MissVisa.modules.disable import DisableAbleCommandHandler
 from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
                       Update)
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
