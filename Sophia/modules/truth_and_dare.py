497e67bf2260a4ea808efa740f0bbf2e5f6898f8
CALLMEVP <82378057+callmevp@users.noreply.github.com>
2021-08-09 19:27:03 +0530

    Update truth_and_dare.py

--- Sophia/modules/truth_and_dare.py
+++ Sophia/modules/truth_and_dare.py
@@ -1,10 +1,10 @@
 import html
 import random
-import Sophia.modules.truth_and_dare_string as truth_and_dare_string
-from Sophia import dispatcher
+import MissVisa.modules.truth_and_dare_string as truth_and_dare_string
+from MissVisa import dispatcher
 
 from telegram import ParseMode, Update, Bot
-from Sophia.modules.disable import DisableAbleCommandHandler
+from MissVisa.modules.disable import DisableAbleCommandHandler
 from telegram.ext import CallbackContext, run_async
 
 @run_async
