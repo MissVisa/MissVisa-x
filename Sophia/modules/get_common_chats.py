326e47f2949d6f266c5691f1472d27ab68326bce
CALLMEVP <82378057+callmevp@users.noreply.github.com>
2021-08-09 15:16:41 +0530

    Update get_common_chats.py

--- Sophia/modules/get_common_chats.py
+++ Sophia/modules/get_common_chats.py
@@ -1,9 +1,9 @@
 import os
 from time import sleep
 
-from Sophia import OWNER_ID, dispatcher
-from Sophia.modules.helper_funcs.extraction import extract_user
-from Sophia.modules.sql.users_sql import get_user_com_chats
+from MissVisa import OWNER_ID, dispatcher
+from MissVisa.modules.helper_funcs.extraction import extract_user
+from MissVisa.modules.sql.users_sql import get_user_com_chats
 from telegram import Update
 from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, Filters
