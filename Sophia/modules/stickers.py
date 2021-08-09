a467de4dff4d915b480bd55cd952a5a6538f4662
CALLMEVP <82378057+callmevp@users.noreply.github.com>
2021-08-09 18:28:31 +0530

    Update stickers.py

--- Sophia/modules/stickers.py
+++ Sophia/modules/stickers.py
@@ -12,8 +12,8 @@ from telegram import TelegramError, Update
 from telegram.ext import run_async, CallbackContext
 from telegram.utils.helpers import mention_html
 
-from Sophia import dispatcher
-from Sophia.modules.disable import DisableAbleCommandHandler
+from MissVisa import dispatcher
+from MissVisa.modules.disable import DisableAbleCommandHandler
 
 combot_stickers_url = "https://combot.org/telegram/stickers?q="
 
