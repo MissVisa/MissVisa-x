d7d7029ec08a7e9283d6367a01b90b4ad08b9eea
CALLMEVP <82378057+callmevp@users.noreply.github.com>
2021-08-09 11:16:13 +0530

    Update bans.py

--- Sophia/modules/bans.py
+++ Sophia/modules/bans.py
@@ -5,7 +5,7 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from Sophia import (
+from MissVisa import (
     DEV_USERS,
     LOGGER,
     OWNER_ID,
@@ -15,8 +15,8 @@ from Sophia import (
     WOLVES,
     dispatcher,
 )
-from Sophia.modules.disable import DisableAbleCommandHandler
-from Sophia.modules.helper_funcs.chat_status import (
+from MissVisa.modules.disable import DisableAbleCommandHandler
+from MissVisa.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     connection_status,
@@ -27,9 +27,9 @@ from Sophia.modules.helper_funcs.chat_status import (
     user_can_ban,
     can_delete,
 )
-from Sophia.modules.helper_funcs.extraction import extract_user_and_text
-from Sophia.modules.helper_funcs.string_handling import extract_time
-from Sophia.modules.log_channel import gloggable, loggable
+from MissVisa.modules.helper_funcs.extraction import extract_user_and_text
+from MissVisa.modules.helper_funcs.string_handling import extract_time
+from MissVisa.modules.log_channel import gloggable, loggable
 
 
 @run_async
