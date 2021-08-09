1f65bba41b671de96aceb34f531d8168337f2d7e
CALLMEVP <82378057+callmevp@users.noreply.github.com>
2021-08-09 19:39:26 +0530

    Update wiki.py

--- Sophia/modules/wiki.py
+++ Sophia/modules/wiki.py
@@ -1,6 +1,6 @@
 import wikipedia
-from Sophia import dispatcher
-from Sophia.modules.disable import DisableAbleCommandHandler
+from MissVisa import dispatcher
+from MissVisa.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 from wikipedia.exceptions import DisambiguationError, PageError
